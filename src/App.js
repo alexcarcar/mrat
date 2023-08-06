3f6a7bdb83685d49cf83763e923055e99acd97bb
ubuntu16 <ubuntu16>
2023-08-06 00:58:13 -0700

    Add menu component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Menu} from "./component/Menu";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
             <br/>&nbsp;<br/>An example React App.
         </p>
+        <Menu/>
       </div>
     );
   }
---
+++ src/component/Menu.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export class Menu extends Component {
+    render() {
+        return (<div>
+            Introduction | Components | Design
+        </div>)
+    }
+}
